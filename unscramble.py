2c68f4eefa265e80e1d859d938c32198c2fc320c
Nolan Bradshaw <nolan.bradshaw@lnisoftware.com>
2020-11-14 17:58:17 -0500

    working logic to unscramble a word

--- unscramble.py
+++ unscramble.py
@@ -1,26 +1,19 @@
 import json 
 
 def unscrambleWord(search_str, key_arr, index):
-    if index >= len(search_str):
-        for i in key_arr:
-            if sorted(search_str) != sorted(key_arr[i]):
-                key_arr.remove(i)
-        return key_arr
-
     # Don't want to manipulate the array
     # we are iterating over.
     key_copy = key_arr.copy()
+    
+    if index >= len(search_str):
+        for i in key_copy:
+            if sorted(search_str) != sorted(i):
+                key_arr.remove(i)
+        return key_arr
 
     for j in key_copy:
         if search_str[index] not in j or len(j) != len(search_str):
             key_arr.remove(j)
-
-    # If nothing has been removed from the array.
-    if key_copy == key_arr:
-        for i in key_arr:
-            if sorted(search_str) != sorted(i):
-                key_arr.remove(i)
-        return key_arr
 
     index += 1
     return unscrambleWord(search_str, key_arr, index)
