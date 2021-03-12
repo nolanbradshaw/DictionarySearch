3d90dc77b338394ba13f0e04752d21d6413baa71
nolanbradshaw <nolanbradshaw22@gmail.com>
2021-03-12 09:15:39 -0500

    readability changes

--- unscramble.py
+++ unscramble.py
@@ -1,16 +1,21 @@
-import json 
+import json
+import timeit
 
 def unscrambleWord(search_str, key_arr, index):
-    # Creating a copy to avoid manipulating an array
-    # we are iterating over.
+    # Creating a copy to iterate over.
     key_copy = key_arr.copy()
     
     if index >= len(search_str):
-        # Check that the sorted versions of each word match (this handles duplicate letters)
-        [key_arr.remove(key) for key in key_copy if sorted(search_str) != sorted(key)]
+        # Check that the sorted versions of each word match.
+        for key in key_copy:
+            if sorted(search_str) != sorted(key):
+                key_arr.remove(key)
         return key_arr
     
-    [key_arr.remove(key) for key in key_copy if search_str[index] not in key or len(key) != len(search_str)]
+    # Remove all words that don't contain the same letters or length.
+    for key in key_copy:
+        if search_str[index] not in key or len(key) != len(search_str):
+            key_arr.remove(key)
 
     index += 1
     return unscrambleWord(search_str, key_arr, index)
@@ -20,5 +25,4 @@ with open('dictionary_compact.json') as dict_file:
     keys = list(data.keys())
 
     search_str = input('Enter the word/letters you want to use: ')
-
     print(unscrambleWord(search_str, keys, 0))
