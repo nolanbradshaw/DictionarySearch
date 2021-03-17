88982c9103798e7b384727b3b040a565993294f3
nolanbradshaw <nolanbradshaw22@gmail.com>
2021-03-17 14:50:35 -0400

    greatly improved performance by removing call to dict.keys(). Passing a dict rather than a list to improve lookup performance

--- unscramble.py
+++ unscramble.py
@@ -1,27 +1,26 @@
 import json
 
-def unscramble_word(search_str, key_arr, index):
+def unscramble_word(search_str, data, index):
     # Creating a copy to iterate over.
-    key_copy = key_arr.copy()
+    data_copy = data.copy()
     
     if index >= len(search_str):
         # Check that the sorted versions of each word match.
-        for key in key_copy:
+        for key in data_copy:
             if sorted(search_str) != sorted(key):
-                key_arr.remove(key)
-        return key_arr
+                del data[key]
+        return data
     
     # Remove all words that don't contain the same letters or length.
-    for key in key_copy:
+    for key in data_copy:
         if search_str[index] not in key or len(key) != len(search_str):
-            key_arr.remove(key)
+            del data[key]
 
     index += 1
-    return unscramble_word(search_str, key_arr, index)
+    return unscramble_word(search_str, data, index)
 
 with open('dictionary_compact.json') as dict_file:
     data = json.load(dict_file)
-    keys = list(data.keys())
 
     search_str = input('Enter the word/letters you want to use: ')
-    print(unscramble_word(search_str, keys, 0))
+    print(list(unscramble_word(search_str, data, 0).keys()))
